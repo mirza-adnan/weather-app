1fbbfc4f8263581684318f5c25ca835f95d73f01
mizzadnan <mirza.adnan2205@gmail.com>
2021-05-07 17:12:03 +0600

    swapped rain for pressure

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("const url = \"https://api.openweathermap.org/data/2.5/weather?q=canada&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric\";\r\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
+eval("const url = \"https://api.openweathermap.org/data/2.5/weather?q=canada&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric\";\r\n\r\n\r\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
 /***/ })
 
