8e3d684da3fe44da3b1228e25abe809dc1e37357
mizzadnan <mirza.adnan2205@gmail.com>
2021-05-07 19:05:19 +0600

    added a module to fetch data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 const url = "https://api.openweathermap.org/data/2.5/weather?q=canada&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric";
-
+fetch(url, {mode: "cors"})
+  .then((res) => res.json())
+  .then((response) => {
+    console.log(response);
+  });
 
---
+++ src/modules/data.js
@@ -0,0 +1,8 @@
+async function getData () {
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=canada&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric";
+  const initialResponse = await fetch(url, {mode: "cors"});
+  const response = await initialResponse.json();
+  return response;
+}
+
+export default getData;
