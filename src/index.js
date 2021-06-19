f4d3ac8f3359928cec544248432b5d7bde87c8b8
mizzadnan <mirza.adnan2205@gmail.com>
2021-06-19 16:01:39 +0600

    moved everything to a single file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import init from "./init";
-
-// initializing everything
-init();
+async function getData(location) {
+	try {
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric`;
+		const initialResponse = await fetch(url, { mode: "cors" });
+		const response = await initialResponse.json();
+		return response;
+	} catch (error) {
+		alert("Unable to find specified city");
+	}
+}
--- src/init.js
+++ src/init.js
@@ -1,9 +1,125 @@
-import {search, displayData, handleKeyboard, removeAnimation} from "./modules/display";
-
 // function to initialise everything
-export default function init () {
-  removeAnimation();
-  search();
-  handleKeyboard();
-  displayData("Dhaka");
+export default function init() {
+	removeAnimation();
+	search();
+	handleKeyboard();
+	displayData("Dhaka");
+}
+
+getData (location) {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric`;
+      const initialResponse = await fetch(url, {mode: "cors"});
+      const response = await initialResponse.json();
+      return response;
+    } catch(error) {
+      alert("Unable to find specified city");
+    }
+    
+}
+
+// add the search funtionality
+function search() {
+	const searchBar = document.querySelector(".search-bar");
+	const searchButton = document.querySelector(".search-button");
+
+	searchButton.addEventListener("click", handleSearch);
+
+	function handleSearch() {
+		if (searchBar.value) {
+			const location = searchBar.value;
+			searchBar.value = "";
+			displayData(location);
+		}
+	}
+}
+
+function displayData(location) {
+	// getting the dom elements
+	const region = document.querySelector(".location");
+	const condition = document.querySelector(".condition").querySelector("span");
+	const temperature = document
+		.querySelector(".temperature")
+		.querySelector("span");
+	const wind = document.querySelector(".wind").querySelector("span");
+	const feelsLike = document
+		.querySelector(".feels-like")
+		.querySelector(".fl-value");
+	const humidity = document.querySelector(".humidity").querySelector("span");
+	const pressure = document.querySelector(".pressure").querySelector("span");
+
+	// receiving the weather data from api
+	getData(location).then((data) => {
+		reAnimate();
+		// updating all the info
+		region.textContent = data.name;
+		condition.textContent = data.weather[0].main;
+		temperature.textContent = String(Math.round(Number(data.main.temp)));
+		wind.textContent = `${data.wind.speed} m/s`;
+		feelsLike.textContent = String(Math.round(Number(data.main.feels_like)));
+		humidity.textContent = `${data.main.humidity}%`;
+		pressure.textContent = `${data.main.pressure} hPa`;
+		changeIcon(data.weather[0].main);
+	});
+}
+
+// changes icon according to the weather
+function changeIcon(data) {
+	const conditionIcon = document.querySelector(".condition").querySelector("i");
+	switch (data) {
+		case "Clear":
+			conditionIcon.className = "far fa-sun";
+			break;
+		case "Rain":
+			conditionIcon.className = "fas fa-cloud-showers-heavy";
+			break;
+		case "Clouds":
+			conditionIcon.className = "fas fa-cloud";
+			break;
+		case "Drizzle":
+			conditionIcon.className = "fas fa-cloud-rain";
+			break;
+		case "Thunderstorm":
+			conditionIcon.className = "fas fa-cloud-showers-heavy";
+			break;
+		case "Snow":
+			conditionIcon.className = "fas fa-snowflake";
+			break;
+	}
+}
+
+// removes css animation class so we can reanimate
+function removeAnimation() {
+	const weatherLeft = document.querySelector(".weather-left");
+	const weatherRight = document.querySelector(".weather-right");
+	const line = document.querySelector(".line");
+
+	[weatherLeft, weatherRight].forEach((weather) => {
+		weather.addEventListener("animationend", () => {
+			weather.classList.remove("fade");
+		});
+	});
+
+	line.addEventListener("animationend", () => {
+		line.classList.remove("elongate");
+	});
+}
+
+// makes the animation re-occur whenever a new location is searched
+function reAnimate() {
+	document.querySelector(".weather-left").classList.add("fade");
+	document.querySelector(".weather-right").classList.add("fade");
+	document.querySelector(".line").classList.add("elongate");
+}
+
+function handleKeyboard() {
+	const searchBar = document.querySelector(".search-bar");
+	const searchButton = document.querySelector(".search-button");
+	window.addEventListener("keydown", (e) => {
+		if (document.activeElement === searchBar) {
+			if (e.key === "Enter" && searchBar.value) {
+				searchButton.click();
+			}
+		}
+	});
 }
