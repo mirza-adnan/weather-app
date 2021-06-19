374829c237c4df873fb9c90038aa6ad11837b64e
mizzadnan <mirza.adnan2205@gmail.com>
2021-06-19 23:00:07 +0600

    put all the js in one file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,132 @@
-async function getData(location) {
-	try {
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric`;
-		const initialResponse = await fetch(url, { mode: "cors" });
-		const response = await initialResponse.json();
-		return response;
-	} catch (error) {
-		alert("Unable to find specified city");
+(function WeatherApp() {
+	const searchBar = document.querySelector('.search-bar');
+	const searchButton = document.querySelector('.search-button');
+	const region = document.querySelector('.location');
+	const condition = document.querySelector('.condition').querySelector('span');
+	const temperature = document
+		.querySelector('.temperature')
+		.querySelector('span');
+	const wind = document.querySelector('.wind').querySelector('span');
+	const feelsLike = document
+		.querySelector('.feels-like')
+		.querySelector('.fl-value');
+	const humidity = document.querySelector('.humidity').querySelector('span');
+	const pressure = document.querySelector('.pressure').querySelector('span');
+	const conditionIcon = document.querySelector('.condition').querySelector('i');
+	const weatherLeft = document.querySelector('.weather-left');
+	const weatherRight = document.querySelector('.weather-right');
+	const line = document.querySelector('.line');
+	const container = document.querySelector('.container');
+
+	init();
+
+	function init() {
+		removeAnimation();
+
+		// event listening
+		window.addEventListener('DDOMContentLoaded', getInitialData());
+		searchButton.addEventListener('click', handleSearch);
+		window.addEventListener('keydown', handleKeyboard);
 	}
-}
+
+	// this function gets the weather data of the location of the user when the website first loads
+	function getInitialData() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition((position) => {
+				const long = position.coords.longitude;
+				const lat = position.coords.latitude;
+				const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric`;
+				getData(url);
+			});
+		} else {
+			const url =
+				'https://api.openweathermap.org/data/2.5/weather?q=Dhaka&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric';
+			getData(url);
+		}
+	}
+
+	async function getData(url) {
+		try {
+			const initialResponse = await fetch(url, { mode: 'cors' });
+			const response = await initialResponse.json();
+			displayData(response);
+		} catch (error) {
+			alert('Cannot find location');
+		}
+	}
+
+	function displayData(data) {
+		reAnimate();
+		console.log('here');
+		region.textContent = data.name;
+		condition.textContent = data.weather[0].main;
+		temperature.textContent = String(Math.round(Number(data.main.temp)));
+		wind.textContent = `${data.wind.speed} m/s`;
+		feelsLike.textContent = String(Math.round(Number(data.main.feels_like)));
+		humidity.textContent = `${data.main.humidity}%`;
+		pressure.textContent = `${data.main.pressure} hPa`;
+		changeIcon(data.weather[0].main);
+	}
+
+	function handleSearch() {
+		if (searchBar.value) {
+			const location = searchBar.value;
+			const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=34fbec596ce6ee0a4ef569154cebd76e&units=metric`;
+			searchBar.value = '';
+			getData(url);
+		}
+	}
+
+	// changes icon according to the weather
+	function changeIcon(data) {
+		switch (data) {
+			case 'Clear':
+				conditionIcon.className = 'far fa-sun';
+				break;
+			case 'Rain':
+				conditionIcon.className = 'fas fa-cloud-showers-heavy';
+				break;
+			case 'Clouds':
+				conditionIcon.className = 'fas fa-cloud';
+				break;
+			case 'Drizzle':
+				conditionIcon.className = 'fas fa-cloud-rain';
+				break;
+			case 'Thunderstorm':
+				conditionIcon.className = 'fas fa-cloud-showers-heavy';
+				break;
+			case 'Snow':
+				conditionIcon.className = 'fas fa-snowflake';
+				break;
+			case 'Haze':
+				conditionIcon.className = 'fas fa-smog';
+		}
+	}
+
+	function removeAnimation() {
+		[weatherLeft, weatherRight].forEach((weather) => {
+			weather.addEventListener('animationend', () => {
+				weather.classList.remove('fade');
+			});
+		});
+
+		line.addEventListener('animationend', () => {
+			line.classList.remove('elongate');
+		});
+	}
+
+	// makes the animation re-occur whenever a new location is searched
+	function reAnimate() {
+		weatherLeft.classList.add('fade');
+		weatherRight.classList.add('fade');
+		line.classList.add('elongate');
+	}
+
+	function handleKeyboard(e) {
+		if (document.activeElement === searchBar) {
+			if (e.key === 'Enter' && searchBar.value) {
+				searchButton.click();
+			}
+		}
+	}
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  }
-}
+	mode: 'production',
+	entry: './src/index.js',
+	devtool: 'inline-source-map',
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+};
